ab4080c180a4627433073e666c40226eb4f82314
KevinYameogo <150480622+KevinYameogo@users.noreply.github.com>
2026-03-17 02:12:44 -0400

    Update Swim.tsx

--- components/Swim.tsx
+++ components/Swim.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Swim = () => {
-  return <div>Swim</div>;
+  return (
+    <div>
+      <h1>How</h1>
+      <div>
+        <h1>Will</h1>
+        life
+      </div>
+    </div>
+  );
 };
 
 export default Swim;
