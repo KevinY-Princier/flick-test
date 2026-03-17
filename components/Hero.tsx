4b466e7b2cd35a813cc03e859c0a03b8d958d797
KevinYameogo <150480622+KevinYameogo@users.noreply.github.com>
2026-03-17 01:50:57 -0400

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 import React from "react";
 
 const Hero = () => {
   return <div>I want this second conflict</div>;
-=======
-const Hero = () => {
-  return <div>I want this first conflict</div>;
->>>>>>> secondary-b
 };
 
 export default Hero;
