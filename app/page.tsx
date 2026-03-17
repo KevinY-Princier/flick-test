cc895404f84374de8b219eb455eef9f3a1adab5c
KevinYameogo <150480622+KevinYameogo@users.noreply.github.com>
2026-03-17 02:13:57 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1>Yours first mine fourth</h1>
-
-      <h1>Yours first mine fourth</h1>
+      <ul></ul>
     </>
   );
 }
