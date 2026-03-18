f96606027334eea7567fec880c393bd1fcc82cc8
KevinY-Princier <kevin.yameogo22@my.stjohns.edu>
2026-03-18 02:28:05 +0000

    Add Princier-b branch features

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <ul>
-        <li>Changes</li>
-      </ul>
+      <div className="container">
+        <h1>Princier-b Branch Features</h1>
+        <ul>
+          <li>Feature A</li>
+          <li>Feature B</li>
+        </ul>
+      </div>
     </>
   );
 }
