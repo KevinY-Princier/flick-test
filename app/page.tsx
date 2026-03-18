67edd9098ee2394f452ce1268a1dd93642c198ac
KevinY-Princier <kevin.yameogo22@my.stjohns.edu>
2026-03-18 02:28:17 +0000

    Add main branch features

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <ul>
-        <li>Changes</li>
-      </ul>
+      <div className="main-content">
+        <h1>Main Branch Content</h1>
+        <p>This is from the main branch</p>
+        <ul>
+          <li>Main Feature 1</li>
+          <li>Main Feature 2</li>
+        </ul>
+      </div>
     </>
   );
 }
